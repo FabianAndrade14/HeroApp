01a487311912399b3488be722bf1b1ac9b7b9f8c
FabianAndrade14 <fabianandresm10@gmail.com>
2024-05-27 16:07:58 -0500

    add(main): Added the html design to the SearchPage component

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,5 +1,60 @@
+import { useForm } from "../../hooks/useForm"
+import { HeroCard } from "../components"
+
 export const SearchPage = () => {
+
+  const { searchText, onInputChange } = useForm({
+    searchText: '',
+  });
+
+  const onSearchSubmit = ( event ) => {
+    event.preventDefault();
+  }
+
   return (
-    <h1>SearchPage</h1>
+    <>
+      <h1>SearchPage</h1>
+      <hr />
+
+      <div className="row">
+        <div className="col-5">
+
+          <h4>Searching</h4>
+          <hr />
+          <form onSubmit={ onSearchSubmit }>
+            <input
+              type="text"
+              placeholder="Search a hero"
+              className="form-control"
+              name="searchText"
+              autoComplete="off"
+              value={ searchText }
+              onChange={ onInputChange }
+            />
+
+            <button className="btn btn-outline-primary mt-1">
+              Search
+            </button>
+          </form>
+        </div>
+
+        <div className="col-7">
+          <h4>Results</h4>
+          <hr />
+
+          <div className="alert alert-primary">
+            Search a hero
+          </div>
+
+          <div className="alert alert-danger">
+            No hero with <b>ABC</b>
+          </div>
+
+          {/* <HeroCard {...hero}></HeroCard> */}
+
+        </div>
+      </div>
+
+    </>
   )
 }
