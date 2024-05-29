7f099bc7ff744a9220c1a6b9b4f40a41f515cb8b
FabianAndrade14 <fabianandresm10@gmail.com>
2024-05-29 17:20:56 -0500

    add(main): added the index.js to the context folder in auth, aso starting to implement active users

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,16 @@
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom"
+import { AuthContext } from "../context";
 
 export const LoginPage = () => {
 
+  const { login } = useContext( AuthContext );
   const navigate = useNavigate();
 
-
   const onLogin = () => {
+
+    login('Fabian Andrade');
+
     navigate('/marvel', {
       replace: true
     });
--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
+
+    const { user } = useContext( AuthContext );
 
     const navigate = useNavigate(); //Este es un customHook creado por la comunidad
 
@@ -52,7 +56,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className="nav-item nav-link text-primary">
-                        Fabian
+                        { user?.name }
                     </span>
 
                     <button
